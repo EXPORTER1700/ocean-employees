15a55d40d088a929a666d92f1462c29f3147d9b9
Ilya Stetsenko <i.stetsenko1998@gmail.com>
2025-08-07 14:40:50 +0300

    project setup

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,9 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
+import simpleImportSort from 'eslint-plugin-simple-import-sort';
+import noRelativeImportPaths from 'eslint-plugin-no-relative-import-paths';
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -11,6 +14,42 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  eslintPluginPrettierRecommended,
+  {
+    plugins: { 'no-relative-import-paths': noRelativeImportPaths },
+    rules: {
+      'no-relative-import-paths/no-relative-import-paths': [
+        'error',
+        {
+          prefix: '@src',
+          rootDir: 'src',
+        },
+      ],
+    },
+  },
+  {
+    plugins: {
+      'simple-import-sort': simpleImportSort,
+    },
+    rules: {
+      'simple-import-sort/imports': [
+        'error',
+        {
+          groups: [
+            ['^react', '^[a-z]'],
+            [
+              '^@',
+              '^\\.\\.(?!/?$)',
+              '^\\.\\./?$',
+              '^\\./(?=.*/)(?!/?$)',
+              '^\\.(?!/?$)',
+              '^\\./?$',
+            ],
+          ],
+        },
+      ],
+    },
+  },
 ];
 
 export default eslintConfig;
